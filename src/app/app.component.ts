2b061369e46db6f17eeb51339a319272be92f817
Souravsaha01041999 <sahasourav630@gmail.com>
2022-12-09 12:37:36 +0530

    09-12-2022 12:37

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent implements OnInit{
   constructor(private login:LoginEvent,private logout:LogOutEvent){}
 
   ngOnInit(): void {
+    this.loadSystem();
+  }
+  
+  //This method used for to get user name from local storage and it will print on navigation
+  loadSystem()
+  {
     let userShow=String(localStorage.getItem("username"));
     if(userShow!="null")
     {
