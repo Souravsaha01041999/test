6e281e75fde59ebe63e6d8007d0b853aa1f416d2
Souravsaha01041999 <sahasourav630@gmail.com>
2022-12-09 15:50:42 +0530

    09-12-2022

--- src/app/addroom/addroom.component.ts
+++ src/app/addroom/addroom.component.ts
@@ -25,20 +25,32 @@ export class AddroomComponent implements OnInit {
 
   onSaveButton()
   {
-    this.http.post("https://workonits.co.in/OFFICE/addProduct.php",{room:this.roomNumber,bed:this.bed,flore:this.flore,det:this.details,image:this.imageLink,price:this.price})
-    .subscribe((res)=>{
-      this.roomNumber="";
-      this.bed="";
-      this.flore="";
-      this.details="";
-      this.imageLink="";
-      this.price="";
+    if(this.roomNumber.length>0&&this.bed.length>0&&this.flore.length>0&&this.details.length>0&&this.imageLink.length>0&&this.price.length>0)
+    {
+      this.http.post("https://workonits.co.in/OFFICE/addProduct.php",{room:this.roomNumber,bed:this.bed,flore:this.flore,det:this.details,image:this.imageLink,price:this.price})
+      .subscribe((res)=>{
+        this.roomNumber="";
+        this.bed="";
+        this.flore="";
+        this.details="";
+        this.imageLink="";
+        this.price="";
+        this.showMessage=true;
+        this.disMsg="Done";
+        setTimeout(()=>
+        {
+          this.showMessage=false;
+        },2000);
+      });
+    }
+    else
+    {
       this.showMessage=true;
-      this.disMsg="Done";
+      this.disMsg="Please enter all details";
       setTimeout(()=>
       {
         this.showMessage=false;
       },2000);
-    });
+    }
   }
 }
